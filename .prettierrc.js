d956a7098ec95200778bcca23a0b54d607d92896
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-04-22 21:10:26 +0200

    Eslint & prettier cleanup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-    semi: true,
-    trailingComma: "all",
-    singleQuote: true,
-    printWidth: 130
-}
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: 'auto',
+};
