3063c51030468567167c0f261915abdc39fb3684
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-04-26 20:46:50 +0200

    Add 'Sign out' behaviour - clean user data

--- src/components/header/HeaderUserPanel.tsx
+++ src/components/header/HeaderUserPanel.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Box, Button, HStack } from '@chakra-ui/react';
 import { BACKEND_LOGIN_URL } from '../../common/constants';
-import { useAppSelector } from '../../redux/hooks';
-import { UserState } from '../../redux/userSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { clearUserData, UserState } from '../../redux/userSlice';
 import { isValidJwt } from '../../common/jwtValidator';
+import { useHistory } from 'react-router-dom';
+import Routes from '../../common/routes';
 
 function HeaderUserPanel(): JSX.Element {
+  const history = useHistory();
+  const dispatch = useAppDispatch();
   const userState: UserState = useAppSelector((state) => state.user);
 
   const redirectToAuth = () => window.location.replace(BACKEND_LOGIN_URL);
+  const signOut = () => {
+    dispatch(clearUserData());
+    history.push(Routes.HOME);
+  };
 
   const { userDetails, jwt } = userState;
   const isLoggedIn: boolean = isValidJwt(jwt);
@@ -17,7 +25,7 @@ function HeaderUserPanel(): JSX.Element {
       {isLoggedIn && (
         <HStack>
           <h1>Hello, {userDetails.displayName}</h1>
-          <Button colorScheme="teal" onClick={() => alert('sign out todo')}>
+          <Button colorScheme="teal" onClick={signOut}>
             Sign Out
           </Button>
         </HStack>
