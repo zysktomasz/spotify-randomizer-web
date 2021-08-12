337ae78a4838b0cc60b3abf28e532a2f7e5d65d9
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-08-12 21:44:35 +0200

    Add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  rules: {
+    // 'linebreak-style': 'off',
+    // 'prettier/prettier': [
+    //     'error',
+    //     {
+    //         endOfLine: 'auto',
+    //     },
+    // ],
+  },
+};
