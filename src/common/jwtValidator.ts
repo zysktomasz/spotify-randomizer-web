a36f3f1e15ab37deff68c083de06425805bb569a
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-08-15 08:44:37 +0200

    Add SuccessfulLoginHandler

---
+++ src/common/jwtValidator.ts
@@ -0,0 +1,49 @@
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+import { UserDetails } from '../redux/userSlice';
+
+const ISSUER = 'spotify-randomizer';
+
+interface CustomJwtPayload {
+  iss?: string;
+  sub?: string;
+  displayName?: string;
+  accessToken?: string;
+}
+
+function getDecodedJwt(jwt: string): CustomJwtPayload | undefined {
+  try {
+    return jwtDecode<JwtPayload>(jwt);
+  } catch (e) {
+    console.error('Unable to decode jwt', e);
+    return undefined;
+  }
+}
+
+export function isValidJwt(jwt: string | undefined): boolean {
+  if (!jwt) {
+    return false;
+  }
+
+  const decodedJwt = getDecodedJwt(jwt);
+
+  if (!decodedJwt) {
+    return false;
+  }
+
+  // todo: extend jwt validation
+  return decodedJwt.iss === ISSUER;
+}
+
+export function getUserDataFromJwt(jwt: string): UserDetails | undefined {
+  const decodedJwt = getDecodedJwt(jwt);
+
+  if (decodedJwt) {
+    return {
+      email: decodedJwt.sub,
+      displayName: decodedJwt.displayName,
+      accessToken: decodedJwt.accessToken,
+    };
+  }
+
+  return undefined;
+}
---
+++ src/component/authentication/SuccessfulLoginHandler.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { Redirect, useLocation } from 'react-router-dom';
+import { getUserDataFromJwt, isValidJwt } from '../../common/jwtValidator';
+import { AuthenticatedUserPayload, setUserData } from '../../redux/userSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+
+function SuccessfulLoginHandler(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const userStateJwt = useAppSelector((state) => state.user.jwt);
+  const query = new URLSearchParams(useLocation().search);
+
+  useEffect(() => {
+    const jwt = query.get('jwt');
+    if (jwt && isValidJwt(jwt)) {
+      const userData = getUserDataFromJwt(jwt);
+      if (userData) {
+        const payload: AuthenticatedUserPayload = {
+          userDetails: userData,
+          jwt,
+        };
+
+        dispatch(setUserData(payload));
+      }
+
+      // todo: do something when unable to get user data from jwt
+    }
+  });
+
+  return isValidJwt(userStateJwt) ? <Redirect to={{ pathname: '/' }} /> : <p>spinner here</p>;
+}
+
+export default SuccessfulLoginHandler;
