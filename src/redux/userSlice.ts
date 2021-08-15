7729ba708e68d6efb569554c8ec7aa939f49fb08
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-08-15 08:44:17 +0200

    Add react-router-dom, react-redux and jwt-decode

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface UserState {
+  userDetails: UserDetails;
+  jwt: string | undefined;
+}
+
+export interface UserDetails {
+  email: string | undefined;
+  displayName: string | undefined;
+  accessToken: string | undefined; // todo: stop storing access token client-side
+}
+
+export interface AuthenticatedUserPayload {
+  userDetails: UserDetails;
+  jwt: string;
+}
+
+const initialState: UserState = {
+  userDetails: {
+    email: undefined,
+    displayName: undefined,
+    accessToken: undefined,
+  },
+  jwt: undefined,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    /* eslint-disable no-param-reassign */
+    setUserData: (state, action: PayloadAction<AuthenticatedUserPayload>) => {
+      const { userDetails, jwt } = action.payload;
+      state.userDetails = userDetails;
+      state.jwt = jwt;
+    },
+    /* eslint-disable no-param-reassign */
+  },
+});
+
+export const { setUserData } = userSlice.actions;
+
+export default userSlice.reducer;
