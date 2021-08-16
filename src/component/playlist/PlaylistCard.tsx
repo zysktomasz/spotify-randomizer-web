9efd9b0238ba968b774d82f8d15df0ef7f2cf89a
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-08-16 20:05:42 +0200

    Retrieve playlist's tracks by playlistId and show them in collapsable panel

--- src/component/playlist/PlaylistCard.tsx
+++ src/component/playlist/PlaylistCard.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Button, ButtonGroup, Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Box, Button, ButtonGroup, Card, CardContent, CardMedia, Collapse, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import TracksPanel from '../track/TracksPanel';
 
 const useStyles = makeStyles((theme) => ({
   cardRoot: {
     display: 'flex',
-    alignItems: 'center',
+    textAlign: 'left',
+    flexDirection: 'column',
     padding: theme.spacing(1.5, 2, 1),
     marginBottom: theme.spacing(2),
+  },
+  cardFlexRow: {
+    display: 'flex',
+    alignItems: 'center',
   },
   albumCover: {
     width: 96,
@@ -25,25 +31,38 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function PlaylistCard(props: { playlistItem: Playlist }): JSX.Element {
-  const { playlistItem } = props;
   const classes = useStyles();
+  const { playlistItem } = props;
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const showSongsButton = <Button onClick={handleExpandClick}>{!expanded ? 'Show songs' : 'Hide songs'}</Button>;
 
   return (
     <Card key={playlistItem.id} className={classes.cardRoot} variant="outlined">
-      <CardMedia className={classes.albumCover} image={playlistItem.coverImageUrl} title="album cover image" />
-      <CardContent className={classes.content}>
-        <Typography component="h5" variant="h5">
-          {playlistItem.name}
-        </Typography>
-        <Typography variant="subtitle1">Songs: {playlistItem.tracksCount}</Typography>
-        <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
-          <Button disabled>Show songs</Button> {/* TODO: implement button */}
-          <Button component={Link} to={{ pathname: playlistItem.webPlayerUrl }} target="_blank">
-            Go to Spotify Player
-          </Button>
-          <Button disabled>Randomize</Button> {/* TODO: implement button */}
-        </ButtonGroup>
-      </CardContent>
+      <Box className={classes.cardFlexRow}>
+        <CardMedia className={classes.albumCover} image={playlistItem.coverImageUrl} title="album cover image" />
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            {playlistItem.name}
+          </Typography>
+          <Typography variant="subtitle1">Songs: {playlistItem.tracksCount}</Typography>
+          <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+            {showSongsButton}
+            <Button component={Link} to={{ pathname: playlistItem.webPlayerUrl }} target="_blank">
+              Go to Spotify Player
+            </Button>
+            <Button disabled>Randomize</Button> {/* TODO: implement button */}
+          </ButtonGroup>
+        </CardContent>
+      </Box>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <TracksPanel playlistId={playlistItem.id} />
+      </Collapse>
     </Card>
   );
 }
---
+++ src/component/track/TracksPanel.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Grid, List } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import Track from './Track';
+import { useAppSelector } from '../../redux/hooks';
+import { BACKEND_URL } from '../../common/constant';
+
+const useStyles = makeStyles((theme) => ({
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function TracksPanel(props: { playlistId: string }): JSX.Element {
+  const classes = useStyles();
+  const { playlistId } = props;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [tracks, setTracks] = useState<Track[]>([]);
+  const userStateJwt = useAppSelector((state) => state.user.jwt);
+
+  useEffect(() => {
+    console.log('TracksPanel component useEffect()');
+
+    // todo: add Authorization header in axios request interceptor
+    // todo: extract to a file that will handle fe -> api calls
+    setIsLoading(true);
+    axios
+      .get(`${BACKEND_URL}/api/spotify/playlist/${playlistId}/tracks`, { headers: { Authorization: `Bearer ${userStateJwt}` } })
+      .then((response) => {
+        const { data } = response;
+        setTracks(data);
+        console.log('response', response);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log('error', error);
+        setIsLoading(false);
+      });
+  }, [playlistId, setTracks, userStateJwt]); // why tho
+
+  return (
+    <>
+      <h2>Tracks for playlist id: {playlistId}</h2>
+      {isLoading && <h2>Loading tracks...</h2>}
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <div className={classes.demo}>
+            <List dense>
+              {tracks.map((track) => (
+                <Track key={track.trackItem.id} track={track} />
+              ))}
+            </List>
+          </div>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
